a947aff7ca3e206834be788b51f089b32434f7be
James McNulty <James McNulty>
2017-03-20 22:23:50 -0500

    added baseline renderer calls

--- molt.rb
+++ molt.rb
@@ -6,25 +6,25 @@ require 'set'
 
 def config_is_valid?(verbose = false)
     begin
-        hash_configs = JSON.parse(File.read('config.json'))
-        hash_config_formats = JSON.parse(File.read('configformat.json'))
+        config_hashs = JSON.parse(File.read('config.json'))
+        config_formats_hash = JSON.parse(File.read('configformat.json'))
     rescue 
         puts 'Could not read config, please restore from default'
         return false
     end
-    return configs_follow_format?(hash_configs, hash_config_formats, verbose)
+    return configs_follow_format?(config_hashs, config_formats_hash, verbose)
 end
 
-def configs_follow_format?(hash_configs, hash_config_formats, verbose = false)
+def configs_follow_format?(config_hashs, config_formats_hash, verbose = false)
     valid = true
-    hash_config_formats.keys.each do |config_name|
+    config_formats_hash.keys.each do |config_name|
         verbose and puts("\n" + config_name + "\n--------------")
-        if !hash_configs[config_name] || hash_configs[config_name] == ""
+        if !config_hashs[config_name] || config_hashs[config_name] == ""
             verbose and puts(config_name + ' missing from config.json')
             valid = false
             next
         end
-        if !value_matches_format?(hash_configs[config_name], hash_config_formats[config_name], verbose)
+        if !value_matches_format?(config_hashs[config_name], config_formats_hash[config_name], verbose)
             valid = false
             next
         end
@@ -33,11 +33,11 @@ def configs_follow_format?(hash_configs, hash_config_formats, verbose = false)
     return valid
 end
 
-def value_matches_format?(value, hash_config_format, verbose = false)
-    if !value_matches_file_type?(value, hash_config_format['file_type'], verbose)
+def value_matches_format?(value, config_hash_format, verbose = false)
+    if !value_matches_file_type?(value, config_hash_format['file_type'], verbose)
         return false
     end
-    if !file_name_matches_extension(value, hash_config_format['extension'], verbose)
+    if !file_name_matches_extension(value, config_hash_format['extension'], verbose)
         return false
     end
     return true
@@ -92,26 +92,23 @@ def run_molt()
         --verbose, -v       Show additional output when running, 
                             can be used with --run or --checkConfig
         --run, -r           Run MOLT according to the configuration provided 
-                            in molt.config; by default, will check the configuration
+                            in molt.config; will check the configuration
                             file before running to ensure it is valid
         --checkConfig, -c   Validate the configuration file, does nothing if 
                             --run is already being used
-        --noCheck, -n       Skip validation of the configuration file
-
         HELP_TEXT
         return
     end
 
-    if !flags_set.include?('--noCheck') && (flags_set.include?('--checkConfig') || flags_set.include?('--run'))
-        if !config_is_valid?(flags_set.include?('--verbose'))
-            puts 'Configuration of molt is invalid'
-            if !flags_set.include?('--verbose') then puts 'Use -v for more information' end
-            return
-        end
+    if !config_is_valid?(flags_set.include?('--verbose'))
+        puts 'Configuration of molt is invalid'
+        if !flags_set.include?('--verbose') then puts 'Use -v for more information' end
+        return
     end
 
     if flags_set.include?('--run')
         puts "Running molt..." 
+        render_using_config()
     end
 end
 
@@ -120,8 +117,7 @@ def get_flags_set_from_arguments()
         [ '--help', '-h', GetoptLong::NO_ARGUMENT],
         [ '--verbose', '-v', GetoptLong::NO_ARGUMENT],
         [ '--run', '-r', GetoptLong::NO_ARGUMENT],
-        [ '--checkConfig', '-c', GetoptLong::NO_ARGUMENT],    
-        [ '--noCheck', '-n', GetoptLong::NO_ARGUMENT]
+        [ '--checkConfig', '-c', GetoptLong::NO_ARGUMENT]
     )
 
     flags_set = Set.new
@@ -131,5 +127,21 @@ def get_flags_set_from_arguments()
     return flags_set
 end
 
+def render_using_config()
+    config_hash = JSON.parse(File.read('config.json'))
+    source_name = "OG"
+    render_command = "aerender.exe -reuse"
+    render_command << " -project \"#{config_hash['ae_project']}\""
+    render_command << " -OMtemplate \"#{config_hash['ae_output_module']}\""
+    render_command << " -comp \"#{config_hash['ae_comp_name']}\""
+    render_command << " -output \"#{get_output_name(config_hash, source_name)}\""
+
+    puts render_command
+    system(render_command)
+end
+
+def get_output_name(config_hash, current_source_name)
+    return config_hash['output_directory'] + config_hash['output_prefix'] + current_source_name
+end
 
 run_molt()
